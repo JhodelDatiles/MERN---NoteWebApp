872367d973b65616c1e905b675e13d1274ed3161
JhodelDatiles <DatilesJhodel.io@gmail.com>
2026-01-01 15:53:17 +0800

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import notesRoute from "./routes/notesRoute.js";
+import {connectDB} from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+//middle ware
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+);
+app.use(express.json());
+app.use(rateLimiter);
+
+// app.use((req, res, next) => {
+//   console.log("We just got a new request!");
+//   next();
+// })
+
+app.use("/api/notes", notesRoute);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on port", PORT);
+  });
+});
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,62 @@
+import {useEffect, useState} from 'react';
+
+import Navbar from '../components/NavBar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
+
+import api from '../lib/axios';
+import toast from "react-hot-toast"
+
+
+const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes,setNotes] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(() =>{
+    const fetchNote = async () => {
+      try {
+        const res = await api.get("/notes");
+        console.log(res.data)
+        setNotes(res.data);
+        setIsRateLimited(false)
+      } catch (error) {
+        console.log("Error fetching notes");
+        console.log(error);
+        if(error.response?.status === 429){
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes!");
+        }
+      } finally {
+          setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [])
+  return (
+    <div className='min-h-screen'>
+      <Navbar/>
+
+      {isRateLimited && <RateLimitedUI/>}
+      <div className='max-w-7xl max-auto p-4 mt-6 ml-auto mr-auto'>
+        
+        {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((Note) => (
+              <NoteCard key={Note._id} note={Note} setNotes={setNotes}/>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+
+  )
+}
+
+export default HomePage;
